737246957d209ff749afc0a4ed0f03430c59b387
Bruno Kilian <bkilian@twilio.com>
2022-03-08 18:03:26 +0100

    bump: plugin CLI

--- src/CrmIntegrationPlugin.tsx
+++ src/CrmIntegrationPlugin.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import * as Flex from '@twilio/flex-ui';
-import { FlexPlugin } from 'flex-plugin';
+import { FlexPlugin } from '@twilio/flex-plugin';
 
 const PLUGIN_NAME = 'CrmIntegrationPlugin';
 
